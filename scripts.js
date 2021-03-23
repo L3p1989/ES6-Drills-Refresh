76817da6499816a08ad09946ca53268b7d610f18
L3p1989 <phogan110@live.com>
2021-03-22 21:44:11 -0700

    added parameter name with default value to favMovie. Changed text for console.log in favMovie

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 //create favMovie function
-let favMovie = (movie = "The Room") => {
-  console.log(`My favorite movie is ${movie}`);
+let favMovie = (name = "Tommy Wiseau", movie = "The Room") => {
+  console.log(`My name is ${name}, and my favorite movie is ${movie}`);
 };
 //call favMovie
 favMovie();
