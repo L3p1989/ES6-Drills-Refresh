45e04d1998b0dbf3aa02fd26cc4d92869eed1e72
L3p1989 <phogan110@live.com>
2021-03-22 22:31:27 -0700

    created getFirstName function

--- scripts.js
+++ scripts.js
@@ -5,3 +5,8 @@ let favMovie = (name = "Tommy Wiseau", movie = "The Room") =>
 favMovie();
 //call favMovie with custom params
 favMovie("L3p", "The Boondock Saints");
+//create getFirstName function
+let getFirstName = (name = "Tommy Wiseau") => {
+  const firstName = name.split(" ");
+  return console.log(firstName[0]);
+};
